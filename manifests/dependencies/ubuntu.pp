1c4fca275e02dd5f02ac837e01bd23256da7d557
Toke Dorph <tod@inpay.com>
2020-02-06 12:56:59 +0100

    .

--- manifests/dependencies/ubuntu.pp
+++ manifests/dependencies/ubuntu.pp
@@ -5,7 +5,7 @@ class rvm::dependencies::ubuntu {
 
   }
 
-  ensure_packages(['build-essential','bison','openssl','libreadline6','libreadline6-dev','curl','git-core',
+  ensure_packages(['build-essential','bison','openssl','libreadline-dev','libreadline6-dev','curl','git-core',
     'zlib1g','zlib1g-dev','libssl-dev','libyaml-dev','libsqlite3-0','libsqlite3-dev','sqlite3','libxml2-dev',
     'autoconf','libc6-dev'])
 
